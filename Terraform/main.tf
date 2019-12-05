10000948a3dc2e9b24df98370973e0e6e7051b49
Ivanna Terletska <i.terletska1@gmail.com>
2019-12-06 01:15:55 +0200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -105,3 +105,15 @@ resource "google_compute_instance" "mongoserver" {
     ]
   } 
 }
+
+resource "google_compute_network_peering" "peering1" {
+  name         = "peering1"
+  network      = google_compute_network.default.self_link
+  peer_network = google_compute_network.serversnetwork.self_link
+}
+
+resource "google_compute_network_peering" "peering2" {
+  name         = "peering2"
+  network      = google_compute_network.serversnetwork.self_link
+  peer_network = google_compute_network.default.self_link
+}
